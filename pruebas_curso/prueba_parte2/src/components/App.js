65f02f7a226febca7b3752c3d3a9f3a3cfa3a404
Saber26 <luissaezd26@gmail.com>
2023-12-09 18:03:56 -0300

    pruebas de formularios y filtrado de elementos

--- pruebas_curso/prueba_parte2/src/components/App.js
+++ pruebas_curso/prueba_parte2/src/components/App.js
@@ -1,14 +1,41 @@
-import Note from './Notes.js';
+import { useState } from "react"
+import Note from "./Notes.js"
 
-const App = ({ notes }) => {
+const App = (props) => {
+    const [notes, setNotes] = useState(props.notes)
+    const [newNote, setNewNote] = useState("")
+    const [showAll, setShowAll] = useState(true)
+    
+    const click = (event) => {
+        event.preventDefault()
+        const noteObject = {
+            id:notes.length+1,
+            content: newNote,
+            date:new Date().toISOString(),
+            important: Math.random()<0.5
+        }
+        setNotes(notes.concat(noteObject))
+        setNewNote("")
+    }
+
+    const handleNoteChange = (event) => {
+        setNewNote(event.target.value)
+    }
+
+    const noteToShowAll = showAll ? notes : notes.filter(note => note.important)
     return (
         <div>
             <h1>Notes</h1>
+            <button onClick={() => setShowAll(!showAll)}>mostrar {showAll? "todas" : "importantes"}</button>
             <ul>
-                {notes.map(note => (
-                    <Note key={note.id} note={note} />
-                ))}
+                {noteToShowAll.map(note=>
+                    <Note key={note.id} note={note}/>
+                )}
             </ul>
+            <form onSubmit={click}>
+                <input value={newNote} onChange={handleNoteChange}/>
+                <button type="submit">Guardar</button>
+            </form>
         </div>
     )
 }
